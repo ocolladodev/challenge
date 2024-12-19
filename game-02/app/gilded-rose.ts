e3baf40cd481a16f2314aa7a1b49dbafc7446dd2
Omar Augusto Collado Vargas <omar.collado@dlwlatam.com>
2024-12-18 22:54:13 -0500

    reto resuelto

--- game-02/app/gilded-rose.ts
+++ game-02/app/gilded-rose.ts
@@ -1,69 +1,111 @@
 export class Item {
-    name: string;
-    sellIn: number;
-    quality: number;
+    constructor(public name: string, public sellIn: number, public quality: number) {}
+}
 
-    constructor(name, sellIn, quality) {
-        this.name = name;
-        this.sellIn = sellIn;
-        this.quality = quality;
+abstract class ItemStrategy {
+    constructor(protected item: Item) {}
+
+    update(): void {
+        this.updateQuality();
+        this.updateSellIn();
+        this.handleExpired();
+    }
+
+    protected abstract updateQuality(): void;
+
+    protected updateSellIn(): void {
+        this.item.sellIn--;
+    }
+
+    protected handleExpired(): void {
+        if (this.item.sellIn < 0) {
+            this.updateExpiredQuality();
+        }
+    }
+
+    protected updateExpiredQuality(): void {
+        // Default behavior: do nothing
+    }
+
+    protected increaseQuality(amount: number = 1): void {
+        this.item.quality = Math.min(50, this.item.quality + amount);
+    }
+
+    protected decreaseQuality(amount: number = 1): void {
+        this.item.quality = Math.max(0, this.item.quality - amount);
+    }
+}
+
+class NormalItemStrategy extends ItemStrategy {
+    protected updateQuality(): void {
+        this.decreaseQuality();
+    }
+
+    protected updateExpiredQuality(): void {
+        this.decreaseQuality();
+    }
+}
+
+class AgedBrieStrategy extends ItemStrategy {
+    protected updateQuality(): void {
+        this.increaseQuality();
+    }
+
+    protected updateExpiredQuality(): void {
+        this.increaseQuality();
+    }
+}
+
+class BackstagePassesStrategy extends ItemStrategy {
+    protected updateQuality(): void {
+        if (this.item.sellIn <= 0) {
+            this.item.quality = 0;
+        } else if (this.item.sellIn <= 5) {
+            this.increaseQuality(3);
+        } else if (this.item.sellIn <= 10) {
+            this.increaseQuality(2);
+        } else {
+            this.increaseQuality();
+        }
+    }
+}
+
+class SulfurasStrategy extends ItemStrategy {
+    protected updateQuality(): void {
+        // Legendary item: quality does not change
+    }
+
+    protected updateSellIn(): void {
+        // Legendary item: sellIn does not change
+    }
+}
+
+class ConjuredItemStrategy extends ItemStrategy {
+    protected updateQuality(): void {
+        this.decreaseQuality(2);
+    }
+
+    protected updateExpiredQuality(): void {
+        this.decreaseQuality(2);
     }
 }
 
 export class GildedRose {
-    items: Array<Item>;
+    constructor(public items: Item[]) {}
 
-    constructor(items = [] as Array<Item>) {
-        this.items = items;
+    updateQuality(): Item[] {
+        this.items.forEach(item => {
+            const strategy = this.getStrategy(item);
+            strategy.update();
+        });
+        return this.items;
     }
 
-    updateQuality() {
-        for (let i = 0; i < this.items.length; i++) {
-            if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if (this.items[i].quality > 0) {
-                    if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                        this.items[i].quality = this.items[i].quality - 1
-                    }
-                }
-            } else {
-                if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1
-                    if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                        if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].sellIn = this.items[i].sellIn - 1;
-            }
-            if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != 'Aged Brie') {
-                    if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].quality > 0) {
-                            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                                this.items[i].quality = this.items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality
-                    }
-                } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1
-                    }
-                }
-            }
-        }
-
-        return this.items;
+    private getStrategy(item: Item): ItemStrategy {
+        if (item.name === "Aged Brie") return new AgedBrieStrategy(item);
+        if (item.name === "Backstage passes to a TAFKAL80ETC concert") return new BackstagePassesStrategy(item);
+        if (item.name === "Sulfuras, Hand of Ragnaros") return new SulfurasStrategy(item);
+        if (item.name === "Conjured") return new ConjuredItemStrategy(item);
+        return new NormalItemStrategy(item);
     }
 }
